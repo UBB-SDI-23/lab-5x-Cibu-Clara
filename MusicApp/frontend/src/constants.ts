e146a1d63e8912bd58c108961d1d1ddf31726a4b
Clara Cibu <125907393+Cibu-Clara@users.noreply.github.com>
2023-04-24 16:40:15 +0300

    Update constants.ts

--- MusicApp/frontend/src/constants.ts
+++ MusicApp/frontend/src/constants.ts
@@ -1,5 +1,3 @@
-const PROD_BACKEND_API_URL = "http://ec2-16-170-171-187.eu-north-1.compute.amazonaws.com/";
-const DEV_BACKEND_API_URL = "http://127.0.0.1:8000/";
+const DEV_BACKEND_API_URL = "http://ec2-13-51-249-183.eu-north-1.compute.amazonaws.com/";
 
-export const BACKEND_API_URL =
-	process.env.NODE_ENV === "development" ? DEV_BACKEND_API_URL : PROD_BACKEND_API_URL;
+export const BACKEND_API_URL = DEV_BACKEND_API_URL;
