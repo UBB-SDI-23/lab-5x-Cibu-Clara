93346e27cb8af7331f0f3072810f199106fed95a
Clara Cibu <125907393+Cibu-Clara@users.noreply.github.com>
2023-04-24 17:41:38 +0300

    Update urls.py

--- MusicApp/backend/MusicApp/urls.py
+++ MusicApp/backend/MusicApp/urls.py
@@ -19,9 +19,9 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('backend.Music.urls')),
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/', include('backend.Music.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
-    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
