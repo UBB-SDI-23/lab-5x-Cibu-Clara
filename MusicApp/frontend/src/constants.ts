2b1f7c6d9be5948c0579027e47e3187fc46f18cc
Clara Cibu <125907393+Cibu-Clara@users.noreply.github.com>
2023-04-24 04:31:32 +0300

    Update constants.ts

--- MusicApp/frontend/src/constants.ts
+++ MusicApp/frontend/src/constants.ts
@@ -1,3 +1,3 @@
-const DEV_BACKEND_API_URL = "http://ec2-16-170-171-187.eu-north-1.compute.amazonaws.com/";
+const DEV_BACKEND_API_URL = "http://ec2-16-170-171-187.eu-north-1.compute.amazonaws.com";
 
 export const BACKEND_API_URL = DEV_BACKEND_API_URL;
