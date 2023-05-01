f1d04fb99dca4472d520a81c946f43476b053ac0
Ubuntu <ubuntu@ip-172-31-7-128.eu-north-1.compute.internal>
2023-05-01 14:20:28 +0000

    done

--- MusicApp/backend/Music_project/settings.py
+++ MusicApp/backend/Music_project/settings.py
@@ -64,24 +64,26 @@ WSGI_APPLICATION = 'Music_project.wsgi.application'
 
 # Dttps://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_musicapp',
+        'USER': 'db_user',
+        'PASSWORD': 'db_password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'db_musicapp',
-#         'USER': 'db_user',
-#         'PASSWORD': 'db_password',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
