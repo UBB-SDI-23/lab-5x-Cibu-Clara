d7be0794d548fecc68f35c76e32fbca880c543af
Cibu-Clara <clara_cibu@yahoo.com>
2023-05-08 14:58:38 +0300

    .

--- MusicApp/frontend/src/components/songs/ShowSongs.tsx
+++ MusicApp/frontend/src/components/songs/ShowSongs.tsx
@@ -10,7 +10,7 @@ import {
     Container,
     IconButton,
     Tooltip,
-    Button, TextField, Toolbar
+    Button, TextField
 } from "@mui/material";
 import {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
@@ -20,24 +20,64 @@ import AddIcon from "@mui/icons-material/Add";
 import ReadMoreIcon from "@mui/icons-material/ReadMore";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { Paginator } from "../pagination/Pagination";
 
 export const ShowSongs = () => {
     const [loading, setLoading] = useState(false);
     const [songs, setSongs] = useState<Song[]>([]);
     const [order, setOrder] = useState("asc");
     let [input, setInput] = useState<number | undefined>();
-    const [currentPage, setCurrentPage] = useState(1);
-	const totalPages = 10000; //Math.ceil(1000000 / 100);
+    const [page, setPage] = useState(1);
+    const [pageSize, setPageSize] = useState(10);
+    const [totalRows, setTotalRows] = useState(0);
+    const crt = (page - 1) * pageSize + 1;
 
-    useEffect(() => {
+    const [isLastPage, setIsLastPage] = useState(false);
+
+    const setCurrentPage = (newPage: number) => {
+        setPage(newPage);
+    }
+
+    const goToNextPage = () => {
+        if (isLastPage) {
+            return;
+        }
+
+        setPage(page + 1);
+    }
+
+    const goToPrevPage = () => {
+        if (page === 1) {
+            return;
+        }
+
+        setPage(page - 1);
+    }
+
+/*    useEffect(() => {
         setLoading(true);
-        fetch(`${BACKEND_API_URL}/songs/?p=${currentPage}`)
+        fetch(`${BACKEND_API_URL}/songs/`)
             .then((response) => response.json())
             .then((data) => {
                 setSongs(data);
                 setLoading(false);
             });
-        }, [])
+        }, [])*/
+    const fetchSongs = async () => {
+        setLoading(true);
+        const response = await fetch(
+          `${BACKEND_API_URL}/songs/?page=${page}&page_size=${pageSize}`
+        );
+        const { count, next, previous, results } = await response.json();
+        setSongs(results);
+        setTotalRows(count);
+        setIsLastPage(!next);
+        setLoading(false);
+      };
+
+      useEffect(() => {
+        fetchSongs();
+      }, [page]);
 
     const sorting = () => {
         if (order === "asc") {
@@ -55,30 +95,9 @@ export const ShowSongs = () => {
         }
     }
 
-	const handlePageChange = (newPage: number) => {
-		setCurrentPage(newPage);
-		setLoading(true);
-		fetch(`${BACKEND_API_URL}/songs/?p=${newPage}`)
-			.then((response) => response.json())
-			.then((data) => {
-				setSongs(data.results);
-				setLoading(false);
-			});
-	};
-
-	const pageNumbers = [];
-	for (
-		let i = Math.max(1, currentPage - 5);
-		i <= Math.min(totalPages, currentPage + 5);
-		i++
-	) {
-		pageNumbers.push(i);
-	}
-
         return (
 		<Container>
 			<h1>All Songs </h1>
-			<label style={{ color: "gray" }}>Current Page: {currentPage}</label>
             <div style={{ display: "flex", alignItems: "center", marginLeft: "900px", marginBottom: "-30px" }}>
             <TextField
                 label="Year of Release"
@@ -94,58 +113,6 @@ export const ShowSongs = () => {
             </div>
 			{loading && <CircularProgress />}
 			{!loading && songs.length === 0 && <p>No songs found</p>}
-            			{!loading && (
-				<Toolbar>
-					<div style={{ width: "1200px" }}>
-						{currentPage > 1 && (
-							<button style={{ margin: "3px" }} onClick={() => handlePageChange(currentPage - 1)}>
-								Previous
-							</button>
-						)}
-						{pageNumbers[0] > 1 && (
-							<>
-								<button style={{ margin: "3px" }} onClick={() => handlePageChange(1)}>1</button>
-								{pageNumbers[0] > 2 && <span style={{ margin: "3px" }} >...</span>}
-							</>
-						)}
-						{pageNumbers.map((pageNumber) => (
-							<button
-								style={{
-									margin: "3px",
-									// backgroundColor: currentPage === pageNumber ? "white" : "",
-									backgroundColor: "white",
-									color: currentPage === pageNumber ? "blue" : "black",
-									pointerEvents: currentPage === pageNumber ? "none" : "auto"
-								}}
-								key={pageNumber}
-								onClick={() => handlePageChange(pageNumber)}
-							>
-								{pageNumber}
-							</button>
-						))}
-						{pageNumbers[pageNumbers.length - 1] <= totalPages - 1 && (
-							<>
-								{pageNumbers[pageNumbers.length - 1] <= totalPages - 2 && (
-									<span style={{ margin: "3px" }}>...</span>
-								)}
-								<button style={{ margin: "3px" }} onClick={() => handlePageChange(totalPages)}>
-									{totalPages}
-								</button>
-							</>
-						)}
-						{currentPage < totalPages && (
-							<button style={{ margin: "3px" }} onClick={() => handlePageChange(currentPage + 1)}>
-								Next
-							</button>
-						)}
-					</div>
-					<IconButton component={Link} sx={{ mr: 3 }} to={`/employees/add`}>
-						<Tooltip title="Add a new employee" arrow>
-							<AddIcon color="primary" />
-						</Tooltip>
-					</IconButton>
-				</Toolbar>
-			)}
 			{!loading && (
 				<IconButton component={Link} sx={{ mr:155 }} to={`/songs/add`}>
 					<Tooltip title="Add a new song" arrow>
@@ -154,6 +121,7 @@ export const ShowSongs = () => {
 				</IconButton>
 			)}
 			{!loading && songs.length > 0 && (
+                <>
 				<TableContainer component={Paper}>
 					<Table sx={{ minWidth: 650 }} aria-label="simple table">
 						<TableHead>
@@ -201,7 +169,19 @@ export const ShowSongs = () => {
 						</TableBody>
 					</Table>
 				</TableContainer>
-			)}
-		</Container>
-	);
+                <Paginator
+                        rowsPerPage={pageSize}
+                        totalRows={totalRows}
+                        currentPage={page}
+                        isFirstPage={page === 1}
+                        isLastPage={isLastPage}
+                        setPage={setCurrentPage}
+                        goToNextPage={goToNextPage}
+                        goToPrevPage={goToPrevPage}
+                />
+			  </>
+        )
+        }
+    </Container>
+    );
 };
