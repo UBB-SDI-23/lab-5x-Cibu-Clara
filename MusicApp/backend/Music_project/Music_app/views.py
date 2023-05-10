9d6c23a387053f25efbe485796370e5450505430
Cibu-Clara <clara_cibu@yahoo.com>
2023-05-11 00:00:20 +0300

    .

--- MusicApp/backend/Music_project/Music_app/views.py
+++ MusicApp/backend/Music_project/Music_app/views.py
@@ -115,7 +115,7 @@ class ArtistList(APIView):
 '''
 
 
-class ArtistListCreateView(generics.ListAPIView):
+class ArtistListCreateView(generics.ListCreateAPIView):
     serializer_class = ArtistSerializer
     pagination_class = CustomPagination
 
