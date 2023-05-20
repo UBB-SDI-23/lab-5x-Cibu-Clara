df6252b487f2770369bff994c135b75d084147fa
Cibu-Clara <clara_cibu@yahoo.com>
2023-05-20 20:05:54 +0300

    register & login

--- MusicApp/backend/Music_project/Music_app/models.py
+++ MusicApp/backend/Music_project/Music_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Song(models.Model):
@@ -7,6 +8,7 @@ class Song(models.Model):
     genre = models.CharField(max_length=100)
     year_of_release = models.IntegerField()
     artists = models.ManyToManyField('Artist', through='PerformsOn')
+    added_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['id']
@@ -21,6 +23,7 @@ class Artist(models.Model):
     country = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     songs = models.ManyToManyField('Song', through='PerformsOn')
+    added_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['id']
@@ -35,6 +38,7 @@ class Album(models.Model):
     label = models.CharField(max_length=100)
     year_of_release = models.PositiveSmallIntegerField()
     main_artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='albums')
+    added_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['id']
@@ -48,9 +52,22 @@ class PerformsOn(models.Model):
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     nr_of_views = models.IntegerField()
     duration = models.CharField(max_length=10)
+    added_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['id']
 
     def __str__(self):
         return self.artist.artist_name + " - " + self.song.song_name
+
+
+class UserProfile(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
+    location = models.CharField(max_length=100)
+    bio = models.CharField(max_length=600)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile", to_field="username")
+    activation_code = models.CharField(max_length=36)
+    activation_expiry_date = models.DateTimeField()
+    active = models.BooleanField()
