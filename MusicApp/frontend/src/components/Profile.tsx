34a8fe926b8218eb694c246f3c9049491c8324d6
Cibu-Clara <clara_cibu@yahoo.com>
2023-05-22 18:08:59 +0300

    a8

--- MusicApp/frontend/src/components/Profile.tsx
+++ MusicApp/frontend/src/components/Profile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Card, CardContent, Container, TextField } from '@mui/material';
 import { FullUser } from '../models/FullUser';
 import { useParams } from 'react-router-dom';
