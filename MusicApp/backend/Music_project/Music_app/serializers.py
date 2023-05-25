c55cd305db6468b2781a2433f24960adbb8bc5e9
Cibu-Clara <clara_cibu@yahoo.com>
2023-05-25 19:57:09 +0300

    .

--- MusicApp/backend/Music_project/Music_app/serializers.py
+++ MusicApp/backend/Music_project/Music_app/serializers.py
@@ -77,7 +77,7 @@ class ArtistSerializer(DynamicFieldsModelSerializer):
     nr_albums = serializers.IntegerField(read_only=True)
     added_by = UserSerializer(read_only=True)
     songs = Song()
-    added_by_id = serializers.IntegerField(write_only=True)
+    added_by_id = serializers.IntegerField()
 
     def validate_email(self, value):
         existing_emails = Artist.objects.filter(email=value)
@@ -103,7 +103,7 @@ class SongSerializer(DynamicFieldsModelSerializer):
     year_of_release = serializers.IntegerField()
     artists = ArtistSerializer(many=True, read_only=True)
     added_by = UserSerializer(read_only=True)
-    added_by_id = serializers.IntegerField(write_only=True)
+    added_by_id = serializers.IntegerField()
 
     def validate_year_of_release(self, value):
         today = datetime.datetime.now()
@@ -128,7 +128,7 @@ class AlbumSerializer(DynamicFieldsModelSerializer):
     main_artist = ArtistSerializer(read_only=True)
     main_artist_id = serializers.IntegerField(write_only=True)
     added_by = UserSerializer(read_only=True)
-    added_by_id = serializers.IntegerField(write_only=True)
+    added_by_id = serializers.IntegerField()
 
     def validate_nr_of_tracks(self, value):
         if value <= 0:
@@ -184,7 +184,7 @@ class PerformsOnSerializer(DynamicFieldsModelSerializer):
     song_id = serializers.IntegerField(write_only=True)
     artist_id = serializers.IntegerField(write_only=True)
     added_by = UserSerializer(read_only=True)
-    added_by_id = serializers.IntegerField(write_only=True)
+    added_by_id = serializers.IntegerField()
 
     def validate_duration(self, value):
         pattern = r'^\d{2}:\d{2}$'
